94d3f03357850e7234a86576cd86254cf9cfefb7
Alexander Gordey <alexnickgordey3@gmail.com>
2022-10-21 16:54:55 +0300

    3 trial

--- lessons/loops/skobki2.py
+++ lessons/loops/skobki2.py
@@ -1,59 +1,20 @@
 s = str(input())
-ls = []
-for c in range(len(s)):
-    if s[c] == '(' or s[c] == '[' or s[c] == '{' or s[c] == ')' or s[c] == ']' or s[c] == '}':
-        ls.append(s[c])    # перезапись в новый список только скобок
-print('только скобки:', ls)
-c = 0
-while c <= len(ls):     # проверка на порядок следования 1
-    if '(' in ls and ')' in ls:
-        if ls[c - 1] == '(' and ls[c] == ')':
-            del ls[ls.index('(')]
-            del ls[ls.index(')')]
-    if '[' in ls and ']' in ls:
-        if ls[c - 1] == '[' and ls[c] == ']':
-            del ls[ls.index('[')]
-            del ls[ls.index(']')]
-    if '{' in ls and '}' in ls:
-        if ls[c - 1] == '{' and ls[c] == '}':
-            del ls[ls.index('{')]
-            del ls[ls.index('}')]
-    c = c + 1
-   # print(ls, 'после удалений парных скобок 1 попытка')
-c = 0
-while c <= len(ls):     # проверка на порядок следования 2
-    if '(' in ls and ')' in ls:
-        if ls[c - 1] == '(' and ls[c] == ')':
-            del ls[ls.index('(')]
-            del ls[ls.index(')')]
-    if '[' in ls and ']' in ls:
-        if ls[c - 1] == '[' and ls[c] == ']':
-            del ls[ls.index('[')]
-            del ls[ls.index(']')]
-    if '{' in ls and '}' in ls:
-        if ls[c - 1] == '{' and ls[c] == '}':
-            del ls[ls.index('{')]
-            del ls[ls.index('}')]
-    c = c + 1
-   # print(ls, 'после удаления пар 2 попытка')
-c = 0
-while c <= len(ls):     # проверка на порядок следования 3
-    if '(' in ls and ')' in ls:
-        if ls[c - 1] == '(' and ls[c] == ')':
-            del ls[ls.index('(')]
-            del ls[ls.index(')')]
-    if '[' in ls and ']' in ls:
-        if ls[c - 1] == '[' and ls[c] == ']':
-            del ls[ls.index('[')]
-            del ls[ls.index(']')]
-    if '{' in ls and '}' in ls:
-        if ls[c - 1] == '{' and ls[c] == '}':
-            del ls[ls.index('{')]
-            del ls[ls.index('}')]
-    c = c + 1
- #  print(' после удаления пар 3 попытка')
+ls = list(s)
+count = 0
+x = False
+for c in range(len(ls)):
+    if ls[0] == '(':
+        if ls[c] == '(':
+            count += 1
+        if ls[c] == ')':
+            count -= 1
+            if count < 0:
+                x = False
+                break
+        if count > 0:
+            x = False
+        else:
+            x = True
+print(x)
 
-if len(ls) == 0:
-    print(len(ls), 'скобки расставлены верно')
-else:
-    print(len(ls), 'скобки расставлены неверно')
+
